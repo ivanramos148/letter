ce593ea31f1fdcee5c523800153333d51f7818f6
Ivan Ramos and Joel Adams <joel@>
2018-01-09 15:37:13 -0800

    add scripts and for form and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var firstNameInput = $("input#firstName").val();
+    var lastNameInput = $("input#lastName").val();
+    var addressInput = $("input#address").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipCodeInput = $("input#zipCode").val();
 
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".address").text(addressInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zipCode").text(zipCodeInput);
+
+    $("#letter").show();
+    $(".formOne") .hide();
+
+    event.preventDefault();
+  });
 });
